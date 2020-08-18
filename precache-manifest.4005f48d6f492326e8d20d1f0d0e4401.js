eda6446f8ae76ba7c1147a43c92ea7d4a32ca2a1
Shreyas <shreyasx@protonmail.ch>
2020-08-19 02:53:59 +0530

    Updates

---
+++ precache-manifest.4005f48d6f492326e8d20d1f0d0e4401.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb19fed9ba2c66014928148e79f69afb",
+    "url": "/cart/index.html"
+  },
+  {
+    "revision": "1ad5b7399d5d1e9e2134",
+    "url": "/cart/static/css/2.98f886d0.chunk.css"
+  },
+  {
+    "revision": "de067be436bcfb67867c",
+    "url": "/cart/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1ad5b7399d5d1e9e2134",
+    "url": "/cart/static/js/2.c07f3111.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cart/static/js/2.c07f3111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de067be436bcfb67867c",
+    "url": "/cart/static/js/main.3a07b176.chunk.js"
+  },
+  {
+    "revision": "4927c2924af25d99dd24",
+    "url": "/cart/static/js/runtime-main.2c967d9a.js"
+  }
+]);
